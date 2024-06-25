fea514d8319de4a264f7daa9ee31b519259a7b21
Vishnukchandran <vishnuvkc@gmail.com>
2024-06-25 10:52:13 +0530

    first

---
+++ README.md
@@ -0,0 +1,64 @@
+# Social_media_application_frontend
+
+### 1.Introduction
+
+This README provides documentation of Social media application frontend.
+
+### 2.Clone the Repository
+
+If you're working with a Git repository, clone it to your local machine using Git commands or download the source code as a ZIP file and extract it to a local directory. Command to clone the repository on the command prompt.
+
+```
+git clone
+```
+
+Navigate to the downloaded folder and open the terminal or command prompt on the same folder path, type
+
+```
+cd social_media_application_capstone_frontend
+```
+
+Once the folder path is changed to the project folder, use the command below to open VS code editor or you can use other code editor installed on your computer and navigate to the social_media_application_capstone_frontend folder.
+
+```
+code .
+```
+
+### 3.Install Dependencies
+
+Once the project folder is opened on the code editor. Run the following command to install project dependencies using npm
+
+```
+npm install
+```
+
+Once the dependencies is installed, run the application on your computer.
+
+### 4.Usage
+
+To start the application, run the following command on the code editor terminal:
+
+```
+npm start
+```
+
+Once the application is running, open your web browser and visit http://localhost:3000 to view the application. You can create the todo list on the browser
+
+### 5.Technologies Used
+
+- React
+- Material UI
+- React Router
+- React Redux
+
+### 6.Contributions
+
+Contributions are welcome! Please feel free to submit a pull request or open an issue for any bugs, feature requests, or suggestions.
+
+### 7.Live page
+
+You can access the live page from [here]()
+
+### 8.Credits
+
+Created by VishnuKChandran.
---
+++ src/scenes/widgets/ChartWidget.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const ChartWidget = ({ userId }) => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+  const token = useSelector((state) => state.token);
+
+  const [user, setUser] = useState(null);
+
+  const getUser = async () => {
+    const response = await fetch(
+      `https://social-media-application-capstone-backend.onrender.com/users/${userId}`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+
+  const { viewedProfile, impressions } = user;
+  const barChartData = {
+    labels: ["Viewed Profile", "Impressions"],
+    datasets: [
+      {
+        label: "Metrics",
+        data: [viewedProfile, impressions],
+        backgroundColor: [
+          "rgba(75, 192, 192, 0.6)",
+          "rgba(153, 102, 255, 0.6)",
+        ],
+        borderColor: ["rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Bar options={options} data={barChartData} />;
+      </FlexBetween>
+    </WidgetWrapper>
+  );
+};
+
+export default ChartWidget;
